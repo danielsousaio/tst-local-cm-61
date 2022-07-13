3cf64cd5128b3f8514871bc506cbc29b0b3b576b
Daniel <daniel.s@crowdbotics.com>
2022-07-13 18:14:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tst_local_cm_61",
+  url: "https://tst_local_cm_61.botics.co",
+  api: "https://tst_local_cm_61.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
